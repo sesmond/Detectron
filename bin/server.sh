d75ce61cf745e4fb84938f756a4a5c7f3ce4f76e
vincent <>
2020-05-08 10:48:17 +0800

    bugfix

--- bin/server.sh
+++ bin/server.sh
@@ -34,7 +34,7 @@ mkdir -p logs
 echo "服务器Single模式启动..."
 # 参考：https://medium.com/building-the-system/gunicorn-3-means-of-concurrency-efbb547674b7
 # worker=3是根据GPU的显存数调整出来的，ration=0.2，大概一个进程占满为2.5G,4x2.5=10G显存
-_CMD="MODE=$MODE gunicorn \
+_CMD="gunicorn \
     --name=table_web_server \
     --workers=1 \
     --bind 0.0.0.0:$PORT \
