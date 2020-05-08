95a1e0ce98a9a7dfc26ee4a9c8690ef66f53222a
vincent <>
2020-05-08 10:12:44 +0800

    up

--- web/static/js/ocr_new_index.js
+++ web/static/js/ocr_new_index.js
@@ -38,12 +38,14 @@ function submit_ocr() {
             'do_verbose':true
         }),
         success: function(res){
-            if (res.code =='0'){
-                // 成功处理逻辑
-                load_result(res)
-            }else{
-                showMessage(res.message)
-            }
+            debugger
+            load_images(res)
+            // if (res.code =='0'){
+            //     // 成功处理逻辑
+            //     load_result(res)
+            // }else{
+            //     showMessage(res.message)
+            // }
          },
         error: function(res){
             // 错误时处理逻辑
@@ -52,6 +54,18 @@ function submit_ocr() {
         });
 }
 
+function load_images(images) {
+    var $table = $("#small_table");
+
+    images.forEach(function (e,i,array) {
+        // console.log(i)
+
+        var $tr ='<tr>'
+              +'<td width="90%" align="left"><img style="min-height:20px;max-width:95%" src="data:image/jpg;base64,'+e+'"></td>'
+            +'</tr>'
+        $table.append($tr)
+    });
+}
 
 function load_result(result) {
     $("#big_image").attr("src","data:image/jpg;base64," + result.image)
